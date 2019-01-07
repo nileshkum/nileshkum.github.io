40b98ba29355c548f122d94c45bd0cc0f8788826
nileshkum <nileshstud@gmail.com>
2019-01-07 16:13:23 +0530

    First ui5 app

---
+++ LlistView/webapp/model/models.js
@@ -0,0 +1,41 @@
+sap.ui.define([
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/Device",
+	"openui5/community/model/firebase/FirebaseModel"
+], function(JSONModel, Device, FirebaseModel) {
+	"use strict";
+
+	return {
+
+		createDeviceModel: function() {
+			var oModel = new JSONModel(Device);
+		
+			oModel.setDefaultBindingMode("OneWay");
+			return oModel;
+		},
+		
+		createFirebaseModel: function(){
+				var config = {
+					    apiKey: "AIzaSyA_Z8DgyBTOuPZ9CBIZidSUA3Ro05uXEp0",
+					    authDomain: "webapp-a0077.firebaseapp.com",
+					    databaseURL: "https://webapp-a0077.firebaseio.com",
+					    projectId: "webapp-a0077",
+					    storageBucket: "webapp-a0077.appspot.com",
+					    messagingSenderId: "699647224453"
+					  };
+					  
+				var oViewModel = new FirebaseModel(null, config);
+			// Login to firebase
+						oViewModel.getFirebasePromise().then(function(firebase){
+						    firebase.auth().signInAnonymously().catch(function(error) {
+						        // Handle Errors here.
+						        //var errorCode = error.code;
+						        //var errorMessage = error.message;
+						     
+						    });
+						});
+					return oViewModel;
+		}
+
+	};
+});
