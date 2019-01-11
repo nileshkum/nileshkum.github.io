9801df4581e7d1f5e214cff59ba1624fdcef42e2
Nilesh Mishra <nileshmishra@Nileshs-MacBook-Air.local>
2019-01-11 23:19:00 +0530

    Sign in Auth

--- model/models.js
+++ model/models.js
@@ -9,11 +9,11 @@ sap.ui.define([
 
 		createDeviceModel: function() {
 			var oModel = new JSONModel(Device);
-		
+
 			oModel.setDefaultBindingMode("OneWay");
 			return oModel;
 		},
-		
+
 		createFirebaseModel: function(){
 				var config = {
 					    apiKey: "AIzaSyA_Z8DgyBTOuPZ9CBIZidSUA3Ro05uXEp0",
@@ -23,15 +23,16 @@ sap.ui.define([
 					    storageBucket: "webapp-a0077.appspot.com",
 					    messagingSenderId: "699647224453"
 					  };
-					  
+
 				var oViewModel = new FirebaseModel(null, config);
 			// Login to firebase
 						oViewModel.getFirebasePromise().then(function(firebase){
-						    firebase.auth().signInAnonymously().catch(function(error) {
+							firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+						  //  firebase.auth().signInAnonymously().catch(function(error) {
 						        // Handle Errors here.
 						        //var errorCode = error.code;
 						        //var errorMessage = error.message;
-						     
+
 						    });
 						});
 					return oViewModel;
