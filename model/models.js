bc4f8b3dcb1c9915d9033e45164012ea88e985c5
Nilesh Mishra <nileshmishra@Nileshs-MacBook-Air.local>
2019-01-11 23:22:48 +0530

    Sigin Error

--- model/models.js
+++ model/models.js
@@ -30,8 +30,8 @@ sap.ui.define([
 							firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
 						  //  firebase.auth().signInAnonymously().catch(function(error) {
 						        // Handle Errors here.
-						        //var errorCode = error.code;
-						        //var errorMessage = error.message;
+						        var errorCode = error.code;
+						        var errorMessage = error.message;
 
 						    });
 						});
